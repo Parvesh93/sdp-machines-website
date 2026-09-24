847af18d845d06a28d9aca9a49d4471807f86519
Parvesh93 <141941315+Parvesh93@users.noreply.github.com>
2026-09-25 00:32:05 +0530

    Align homepage section order with approved brief

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/components/website/hero/hero";
 import { InstallationsPreview } from "@/components/website/home/installations-preview";
-import { MachineDepthReveal } from "@/components/website/home/machine-depth-reveal";
 import { MachineShowcase } from "@/components/website/home/machine-showcase";
 import { ServiceNetwork } from "@/components/website/home/service-network";
 import { Testimonials } from "@/components/website/home/testimonials";
@@ -9,7 +8,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <MachineDepthReveal />
       <MachineShowcase />
       <ServiceNetwork />
       <InstallationsPreview />
